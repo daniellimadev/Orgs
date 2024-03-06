0a1d83bb2b39eb9d6ecf332d43dd3fb680276323
Daniel Lima <pdaniel643@gmail.com>
2024-03-06 16:51:43 -0300

    Initial commit

---
+++ app/src/main/java/com/daniellimadev/orgs/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.daniellimadev.orgs
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Welcome to Orgs!", Toast.LENGTH_SHORT).show()
+    }
+
+}
