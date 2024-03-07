0fd1303c1810c9560154afe4c22183ce8476d0d3
Daniel Lima <pdaniel643@gmail.com>
2024-03-07 14:43:06 -0300

    Creating the first Activity layout

--- app/src/main/java/com/daniellimadev/orgs/MainActivity.kt
+++ app/src/main/java/com/daniellimadev/orgs/MainActivity.kt
@@ -2,13 +2,15 @@ package com.daniellimadev.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Welcome to Orgs!", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_main)
     }
 
 }
