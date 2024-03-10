9d732d4befa6af8ee12faa2fb49c69718e8a55c8
Daniel Lima <pdaniel643@gmail.com>
2024-03-10 20:50:40 -0300

    Implementing the form

--- app/src/main/java/com/daniellimadev/orgs/model/Product.kt
+++ app/src/main/java/com/daniellimadev/orgs/model/Product.kt
@@ -2,7 +2,7 @@ package com.daniellimadev.orgs.model
 
 import java.math.BigDecimal
 
-class Product(
+data class Product(
         val name: String,
         val description: String,
         val value: BigDecimal
--- app/src/main/java/com/daniellimadev/orgs/ui/activity/FormProductActivity.kt
+++ app/src/main/java/com/daniellimadev/orgs/ui/activity/FormProductActivity.kt
@@ -2,8 +2,42 @@ package com.daniellimadev.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import com.daniellimadev.orgs.R
+import com.daniellimadev.orgs.model.Product
+import java.math.BigDecimal
 
 class FormProductActivity : AppCompatActivity(R.layout.activity_form_product) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val buttonSave = findViewById<Button>(R.id.button_save)
+        buttonSave.setOnClickListener {
+            val fieldName = findViewById<EditText>(R.id.name)
+            val name = fieldName.text.toString()
+            val findDescription = findViewById<EditText>(R.id.description)
+            val description = findDescription.text.toString()
+            val findValue = findViewById<EditText>(R.id.value)
+            val textInValue = findValue.text.toString()
+            val value = if (textInValue.isBlank()) {
+                BigDecimal.ZERO
+            } else {
+                BigDecimal(textInValue)
+            }
+
+
+            val productNew = Product(
+                name = name,
+                description = description,
+                value = value
+            )
+
+
+            Log.i("FormProduct", "onCreate: $productNew")
+        }
+    }
 
 }
